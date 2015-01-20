5e31b9c5b106b75b2994b2cdf408680f796e986e
xu.zheng <xu.zheng@southgis.com>
2015-01-20 11:30:08 +0800

    add ex1.4 ex1.5

---
+++ ch01/ex1.4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+  std::cout<<"请输入两个数:"<<std::endl;
+		float v1=0.0,v2=0.0;
+		std::cin>>v1>>v2;
+		std::cout<<"两个数的乘积等于:"<<v1*v2<<std::endl;
+		return 0;
+}
---
+++ ch01/ex1.5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+  std::cout<<"请输入两个数:";
+		std::cout<<std::endl;
+		float v1=0.0,v2=0.0;
+		std::cin>>v1;
+		std::cin>>v2;
+		std::cout<<"两个数的乘积等于:";
+		std::cout<<std::endl;
+		std::cout<<v1*v2;
+		std::cout<<std::endl;
+		return 0;
+}
